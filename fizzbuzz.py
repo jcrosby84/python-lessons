e5fdacf797b861c94274e640388182f895f92de1
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-03-16 18:19:20 -0400

    Wrote fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python
 # Write a fizz buzz program in python.
+
+import sys
+
+start = int(sys.argv[1])
+end = int(sys.argv[2])
+
+while start < end:
+    if start % 3 == 0 and start % 5 == 0:
+        print "FizzBuzz"
+    elif start % 3 == 0:
+        print "Fizz"
+    elif start % 5 == 0:
+        print "Buzz"
+    else:
+        print start
+    start += 1
